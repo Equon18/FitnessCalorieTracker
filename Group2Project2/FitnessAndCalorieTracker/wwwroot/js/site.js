93caaebae68af5a2291ad711abd4aba0f3d31b0d
Ji-Soo-Yoon <jyoon1@my.westga.edu>
2026-05-11 02:06:37 -0400

    Add contributor information to site.js
    Added contributor information to the site.js file.

--- Group2Project2/FitnessAndCalorieTracker/wwwroot/js/site.js
+++ Group2Project2/FitnessAndCalorieTracker/wwwroot/js/site.js
@@ -1,3 +1,5 @@
+Contributor: Jisoo Yoon
+
 // Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
