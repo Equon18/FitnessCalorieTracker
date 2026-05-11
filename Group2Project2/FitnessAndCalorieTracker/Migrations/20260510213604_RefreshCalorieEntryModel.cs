245fc3a6ed5a0c9c45cefddb1355f58282d154a0
Ji-Soo-Yoon <jyoon1@my.westga.edu>
2026-05-11 01:14:43 -0400

    Create RefreshCalorieEntryModel migration
    Add a migration for refreshing the CalorieEntry model.

---
+++ Group2Project2/FitnessAndCalorieTracker/Migrations/20260510213604_RefreshCalorieEntryModel.cs
@@ -0,0 +1,24 @@
+Contributor: Jisoo Yoon
+
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FitnessAndCalorieTracker.Migrations
+{
+    /// <inheritdoc />
+    public partial class RefreshCalorieEntryModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
